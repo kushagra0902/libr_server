0f47c316f3248efc7647f211259fe52b83edbf11
kushagra0902 <kinrakushagra@gmail.com>
2025-08-23 16:48:42 +0530

    added pinger for relay

--- index.js
+++ index.js
@@ -9,24 +9,17 @@ const db_uri = process.env.MONGO_URI;
 
 await connect_db(db_uri, db_name);
 
-
-function startPinger() {
-  setInterval(() => {
-    const req = https.request('https://libr-relay-y7ib.onrender.com', { method: 'GET' }, (res) => {
-      console.log(`Status Code: ${res.statusCode}`);
-      
-    });
-
-    req.on('error', (err) => {
-      console.error('Ping failed:', err.message);
-    });
-
-    req.end();
-  }, 10 * 60 * 1000); // every 10 minutes
+async function pinger(){
+  console.log("Pinging Relay")
+  setInterval(()=>{
+    https.get("https://libr-relay-y7ib.onrender.com",(res)=>{
+      console.log(`Response Status: ${res.statusCode}`)
+    })
+  },10*60*1000)
+  console.log("Pinged successfully")
 }
 
-startPinger();
-
+pinger()
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Server started on port:`, process.env.PORT || 8080);
 });
